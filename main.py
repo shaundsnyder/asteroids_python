0ae1d62193a84498a6078ba04ad59029181a9dea
Shaun <shaundsnyder@gmail.com>
2026-05-09 00:03:56 -0500

    Starting asteroids project

---
+++ main.py
@@ -0,0 +1,27 @@
+import pygame
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from logger import log_state
+
+
+
+def main():
+	pygame.init()
+	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	print(f"Starting Asteroids with pygame version: {pygame.version.vernum}")
+	print(f"Screen width: {SCREEN_WIDTH}")
+	print(f"Screen height: {SCREEN_HEIGHT}")
+
+	while True:
+		log_state()
+		for event in pygame.event.get():
+    			pass
+
+		screen.fill((0, 0, 0))
+		pygame.display.flip()	
+
+	
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+        			return
+if __name__ == "__main__":
+    main()
